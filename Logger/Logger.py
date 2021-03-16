c63bd96b8bc74a36a8226d87e814e2f58af5a11c
Casper Sejrup <sejrup112@gmail.com>
2021-03-16 10:26:59 +0100

    Another test commit from pi

--- Logger/Logger.py
+++ Logger/Logger.py
@@ -1,3 +1,23 @@
 import RPi.GPIO as GPIO
 import random
 from time import sleep
+
+
+
+
+
+#this is main function
+def main():
+    try:
+        while True:
+    except KeyboardInterrupt:
+        print("Program Terminated")
+    finally:
+        GPIO.cleanup()
+
+
+
+
+#Runs Main Function
+if __name__=="__main__":
+    main()
