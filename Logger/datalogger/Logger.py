783794a5155745aa54a0dfb7cc8554cd110cb0ab
Casper Sejrup <sejrup112@gmail.com>
2021-03-25 00:28:48 +0100

    Code clean up.

--- Logger/datalogger/Logger.py
+++ Logger/datalogger/Logger.py
@@ -33,14 +33,13 @@ YELLOW = 16
 delay = 1
 max_hum = 520.0 #Maximum value of Humidity, sensor calibrated. 
 minimumtemp = 0
-plantid = ""
-loggerid = ""
+plant_id = ""
+logger_id = ""
 
 #SPI Module Setup
 spi = spidev.SpiDev() #New Object
 spi.open(0,0)
 spi.max_speed_hz = 1000000
-
 
 def initialize_gpio():
     GPIO.setmode(GPIO.BCM)
@@ -48,6 +47,22 @@ def initialize_gpio():
     GPIO.setup(RED,GPIO.OUT,initial=GPIO.LOW) 
     GPIO.setup(YELLOW,GPIO.OUT,initial=GPIO.LOW) 
     GPIO.setwarnings(False)
+
+#Function that sends a GET Request to API to recieve a logger to check whether the logger exists so the
+#Program should not post/create a new logger. To be implemented. 
+async def get_data(_idd):
+    exists_logger_id = ""
+    resp = await ApiCalls.get_logger(_idd)
+    httpcode = resp.status_code
+    if httpcode != 200:
+        raise APIError('GET /logger/{}'.format(httpcode))
+    else:
+        print("Logger exists: "+ str(httpcode))
+        rawtext = json.dumps(resp.json(),sort_keys=True,indent=4)
+        jprint(resp.json())
+        json_logger_object = json.loads(rawtext)
+        exists_logger_id = json_logger_object["_id"]
+    return exists_logger_id
 
 #Function to Read from YL69 (soil sensor) returns percentage humidity
 async def read_soil_humd(channel):
@@ -69,18 +84,9 @@ async def read_temp_humd():
     time.sleep(delay)
     return air_temperature, air_humidity
 
-#Function that sends a GET Request to API to recieve a logger
-async def get_data(_idd):
-    resp = ApiCalls.get_logger(_idd)
-    httpcode = resp.status_code
-    if httpcode != 200:
-        raise APIError('GET /logger/{}'.format(httpcode))
-    else:
-        jprint(resp.json())
-
 #Function that sends a POST request to API to create a logger  
 async def post_logger():
-    global loggerid
+    global logger_id
     resp = await ApiCalls.post_logger()
     httpcode = resp.status_code
     if httpcode!= 201:
@@ -88,39 +94,44 @@ async def post_logger():
     else:
         print("Logger Created:"+ str(httpcode))
         rawtext = json.dumps(resp.json(),sort_keys=True,indent=4)
-        json_plant_object = json.loads(rawtext)
-        loggerid = json_plant_object["_id"]
-        print("Logger posted and ID recieved: "+ loggerid +' with status code: '+ str(resp.status_code))
-    #return loggerid
+        json_logger_object = json.loads(rawtext)
+        logger_id = json_logger_object["_id"]
+        print("Logger posted and ID recieved: "+ logger_id +' with status code: '+ str(resp.status_code))
+    return logger_id
 
 #Function that sends a POST request to API to create a log
 async def post_log():
     air_temp, air_hum = await read_temp_humd()
     soil_hum = await read_soil_humd(0)
-    #plant_id = await get_plantid_by_loggerid(0)
-    print(plantid)
-    resp = await ApiCalls.post_log(air_temp,air_hum,soil_hum,plantid)
+    plant_id = await get_plant_id()
+
+    print(plant_id)
+    resp = await ApiCalls.post_log(air_temp,air_hum,soil_hum,plant_id)
     httpcode = resp.status_code
     if httpcode != 201:
         raise APIError('POST /log/ {}'.format(httpcode))
     else:
         print("Log posted: "+ str(resp.status_code))
+        ##Calls jprint function to print the created LOG in terminal.
         jprint(resp.json())
     
 #Function thats send a GET request to API to GET a plant_ID by LoggerID    
-async def get_plantid_by_loggerid():
-    global plantid
-    print(loggerid)
-    resp = await ApiCalls.get_plantby_loggerid(loggerid)
+async def get_plant_id():
+    global plant_id
+    global logger_id
+    ##Calls the post_logger() function to POST a Logger to generate a Logger_ID
+    logger_id = await post_logger()
+    time.sleep(10)
+    resp = await ApiCalls.get_plantby_loggerid(logger_id)
     httpcode = resp.status_code
     if httpcode != 200:
         raise APIError('GET /logger/{}'.format(httpcode))
     else:
         rawtext = json.dumps(resp.json(),sort_keys=True,indent=4)
         json_plant_object = json.loads(rawtext)
-        plantid = json_plant_object["_id"]
+        plant_id = json_plant_object["_id"]
         print("Plant ID recieved: "+ str(resp.status_code))
-    #return plantid
+    return plant_id
 
 def raise_warning():
     GPIO.output(RED,1)
@@ -133,24 +144,18 @@ def testprint():
 def jprint(obj):
     text = json.dumps(obj,sort_keys=True,indent=4)
     print(text)
+
 #This is main function
 async def main():
     try:
         initialize_gpio()
-        #schedule.every(5).seconds.do(testprint)
-        schedule.every().hour.do(post_logger)
-        time.sleep(delay)
-        schedule.every().hour.do(get_plantid_by_loggerid)
-        time.sleep(delay)
-        schedule.every().hour.do(post_log)
+        #time.sleep(delay)
+        #schedule.every().hour.do(get_plantid_by_loggerid) The Scheduler assigns that "post_logger" should be done every hour.
+        #time.sleep(delay)
+        #schedule.every().hour.do(post_log) The Scheduler assigns that "post_logger" should be done every hour.
         while True:
             val = await read_soil_humd(0)
             schedule.run_pending()
-            await read_temp_humd()
-            await post_logger()
-            time.sleep(10)
-            await get_plantid_by_loggerid()
-            time.sleep(2)
             await post_log()
             if(val!=0):
                 if(val > 50):
@@ -188,6 +193,3 @@ class Status(enum.Enum):
 
 asyncio.ensure_future(main())
 loop.run_forever()
-#Invokes Main Method
-#if __name__=="__main__":
- #   main()
